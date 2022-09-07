5cd7bc184d6955d0a7f3588b69733520dea9f43c
Pawel Kolacinski <pawel.kolacinski@gmail.com>
2022-09-07 15:26:34 +0200

    Added better date display, 'load more' button and better Channel view

--- src/components/Channel.js
+++ src/components/Channel.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { Alert, Box } from '@mui/material'
+import React, { useEffect, useState } from 'react'
+import { Alert, Box, Button } from '@mui/material'
 import useRSSFetch from '../hooks/useRSSFetch'
 import CircularProgress from '@mui/material/CircularProgress'
 
@@ -7,15 +7,17 @@ import ChannelArticle from './ChannelArticle'
 
 export default function Channel({ channel }) {
   const { fetchRSSChannel, isLoading, error, data } = useRSSFetch(channel.url)
+  const [showArticlesCount, setShowArticlesCount] =useState(4)
 
   useEffect(() => {
     fetchRSSChannel()
   }, [fetchRSSChannel])
 
   const channelData = data?.channel
+  console.log(channelData)
 
   return (
-    <Box sx={{ m: 4, p: 4 }}>
+    <Box sx={{ m: {xs:0, md:0} , p: {xs:0, md:0} }}>
       {error && <Alert severity="error">There is an error: {error} </Alert>}
       {isLoading && (
         <Box sx={{ textAlign: 'center' }}>
@@ -24,10 +26,13 @@ export default function Channel({ channel }) {
       )}
       {channelData &&
         channelData.item
-          .filter((item, index) => index <= 3)
-          .map((article) => (
-            <ChannelArticle article={article} channelInfo={channelData} />
+          .filter((item, index) => index < showArticlesCount)
+          .map((article, index) => (
+            <ChannelArticle article={article} channelInfo={channelData} key={article.link}/>
           ))}
+        {channelData && (
+            <Button sx={{display:'block',mx:'auto'}} variant="contained" onClick={() => setShowArticlesCount(count => count + 4)}>Load more from {channelData.image.title ?? 'this channel'} </Button>
+        )}
     </Box>
   )
 }
--- src/components/ChannelArticle.js
+++ src/components/ChannelArticle.js
@@ -1,7 +1,12 @@
 import { Paper, Box, Link, Typography } from '@mui/material'
-import Image from '../components/Image'
+import { format} from 'date-fns'
+
 
 export default function ChannelArticle({ channelInfo, article }) {
+
+  const pubDate = format(new Date(article.pubDate), 'dd-MM-yyyy HH:mm:ss')
+    
+
   return (
     <Paper
       elevation={3}
@@ -9,17 +14,19 @@ export default function ChannelArticle({ channelInfo, article }) {
         display: 'flex',
         m: 2,
         justifyContent: 'space-between',
-        flexDirection: { xs: 'column', md: 'row' },
+        flexDirection: { xs: 'column', sm: 'row' },
       }}
       component="article"
     >
-      <Link href={channelInfo.image.link} sx={{ flexGrow: 0 }}>
-        <Image src={channelInfo.image.url} alt={channelInfo.image.title} />
+      <Link href={channelInfo.image.link} sx={{ flexGrow: 0, flexShrink:0,  flexBasis: '15%',  display:'flex', alignItems:'center',justifyContent:'center',p:2 }} >
+        
+            <img src={channelInfo.image.url} alt={channelInfo.image.title} style={{width:'100%',maxWidth:'150px'}}  />
+        
       </Link>
       <Box sx={{ flexGrow: 1, p: 2 }}>
         <Link href={article.link} underline="none">
           <Typography variant="p">
-            Publication date: {article.pubDate}
+            Publication date: {pubDate}
           </Typography>
           <Typography variant="h6" component="h2">
             {article.title}
